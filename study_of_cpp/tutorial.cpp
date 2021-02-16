5b9f1aaca7888d1bee0e9794655da5ab613c921e
solidcellaMoon <solidcella@gmail.com>
2021-02-16 23:46:03 +0900

    bool inline

--- study_of_cpp/tutorial.cpp
+++ study_of_cpp/tutorial.cpp
@@ -1,37 +1,10 @@
 #include <iostream>
-#include <cstring>
-
+#define PI 3.141592
+#define CU(x) ((x)*(x)*(x))
 using namespace std;
 
-class Myclass{
-    private:
-        char *str;
-    public:
-        Myclass(const char *aStr){
-            str = new char[strlen(aStr)+1];
-            strcpy(str, aStr);
-        }
-        Myclass(const Myclass& mc){ //추가한 내용
-            str = new char[strlen(mc.str)+1];
-            strcpy(str, mc.str);
-        }
-        ~Myclass(){
-            delete []str;
-            cout << "~Myclass() called" << endl;
-        }
-        void ShowData(){
-            cout << "str: " << str << endl;
-        }
-};
-
-
 int main() {
-
-    Myclass mc1("MyClass!");
-    Myclass mc2 = mc1;
-
-    mc1.ShowData();
-    mc2.ShowData();
-
+    cout << PI << endl;
+    cout << CU(3.4) << endl;
     return 0;
 }
